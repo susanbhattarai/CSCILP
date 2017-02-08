578226b17aab38244e53d9e4279956459f97322c
Susan Bhattarai <susan.bhattarai@bison.howard.edu>
2017-02-08 10:43:08 -0500

    Reformatted the indentation and comment styling

--- lp.pl
+++ lp.pl
@@ -3,6 +3,7 @@ Predicate Name: sum-up-numbers-simple
 Parameters: L is list and N is a sum-up-numbers-simple
 Return a Boolean
 **/
+
 sum-up-numbers-simple([], 0).  %Empty List with Sum 0
 sum-up-numbers-simple([H|T], N) :- 
     number(H),
@@ -20,6 +21,7 @@ If the head is a number, use the same logic as question1
 If the head is a list, add the predicate with head and the rest of list
 Else, ignore the head value
 **/
+
 sum-up-numbers-general([], 0). %Empty list with sum 0
 %If head is a list, then use the Predicate sum-up-numbers-simple on head and add 
 sum-up-numbers-general([H|T], N) :-
@@ -36,13 +38,13 @@ sum-up-numbers-general([H|T], N) :-
 sum-up-numbers-general([_|T], N) :-
     sum-up-numbers-general(T, N).
 
-
-/**
+/*
 Question3:
 PredicateName: min-above-min
 Params: L1, L2
 Output: Minimum element of L1 > Minimum element of L2
-**/
+*/
+
 min-above-min([A], [], A) :-
     number(A).
 
@@ -50,23 +52,24 @@ min-above-min([A], [], A) :-
 min-above-min(L1, L2, N) :-
     length(L2, 0),
     min-val(L1, N).
-
+%If the head is a number and greater than min-val, use the predicate larger-num
 min-above-min([H|T], L2, H) :-
     number(H),
     min-val(L2, MinL),
     MinL < H,
     larger-num(T, MinL, H, H).
-
+%Else, the answer is the predicate with tail list
 min-above-min([_|T], L2, Ans) :-
     min-above-min(T, L2, Ans).
 
 
 
-/**Question4
+/*Question4
 PredicateName: common-unique-elements\
 Parameters: L1, L2, N
 Result: Boolean
-**/
+*/
+
 common-unique-elements([], _, []). %Rule Statement
 common-unique-elements([H|T], L2, [H|Tail]) :- %If member and only unique in list, add to the result list
     not(is-member(H, T)),
@@ -82,11 +85,12 @@ common-unique-elements([H|T], L2, ConcatList) :- %If the head is a list
 common-unique-elements([_|T], L2, Tail) :-   %If not, ignore the head
     common-unique-elements(T, L2, Tail).
 
-/**Helper functions(Handles for nested lists)
+/*Helper functions(Handles for nested lists)
 PredicateName: is-member
 Parameters: N is a number and L is a list
 Return a boolean
-**/
+*/
+
 is-member(X, [X|_]). %Rule Statement
 is-member(X, [K|Z]) :-
     is_list(K), %If K is a list, check the presence of X in K too.
@@ -95,12 +99,12 @@ is-member(X, [K|Z]) :-
 is-member(X, [_|Z]) :-
     is-member(X, Z). %If does not match with head, look at list
 
-
 /*
 PredicateName: min-val
 Parameters: L is a list
 Output: smallest element of L
 */
+
 min-val([B], B) :-
     number(B).
 %If the first element is less than the second element,pass into recursion
@@ -129,6 +133,7 @@ PredicateName: larger-num
 Params: A list(L), a number(constant) , another number within list
 Output: Returns the number greater than the constant
 */
+
 larger-num([], _, Num, Num). 
 %If the head is greater than const and less than num, set as target
 larger-num([H|T], Const, Num, H) :-
